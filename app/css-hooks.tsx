3980296a71855ee14fcc0c8bc29619e5c772bae1
markdalgleish <mark.john.dalgleish@gmail.com>
2023-12-15 15:58:18 +1100

    Debug hooks in dev

--- app/css-hooks.tsx
+++ app/css-hooks.tsx
@@ -3,15 +3,18 @@ import { createHooks } from "@css-hooks/react";
 const container = "__container__";
 const containerStyles = `:root,.${container}{container-type:inline-size;container-name:${container}}`;
 
-const [hooks, css] = createHooks({
-  ":container(640px)": `@container ${container} (min-width: 640px)`,
-  ":container(1024px)": `@container ${container} (min-width: 1024px)`,
-  ":window(640px)": `@media screen and (min-width: 640px)`,
-  ":window(1024px)": `@media screen and (min-width: 1024px)`,
-  ":light": "@media (prefers-color-scheme: light)",
-  ":dark": "@media (prefers-color-scheme: dark)",
-  ":hover": ":hover",
-});
+const [hooks, css] = createHooks(
+  {
+    ":container(640px)": `@container ${container} (min-width: 640px)`,
+    ":container(1024px)": `@container ${container} (min-width: 1024px)`,
+    ":window(640px)": `@media screen and (min-width: 640px)`,
+    ":window(1024px)": `@media screen and (min-width: 1024px)`,
+    ":light": "@media (prefers-color-scheme: light)",
+    ":dark": "@media (prefers-color-scheme: dark)",
+    ":hover": ":hover",
+  },
+  { debug: process.env.NODE_ENV !== "production" }
+);
 
 export { css };
 export const classes = { container };
